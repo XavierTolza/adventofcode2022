119f85863139b81672306763b3cad18965d07d5c
Xavier TOLZA <xavier.tolza.external@airbus.com>
2022-12-14 09:51:12 +0100

    day 10

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
+from glob import glob
 from importlib import import_module
 from os import getenv, listdir, makedirs
-from os.path import dirname, isdir, isfile, join
+from os.path import basename, dirname, isdir, join
 
 from aocd import get_data
 
 session = getenv("AOC_SESSION")
 
-result_path="results"
-run_demo:bool=eval(getenv("RUN_DEMO","False").capitalize())
+result_path = "results"
+run_demo: bool = eval(getenv("RUN_DEMO", "False").capitalize())
 
-for year in listdir(path='aoc')[::-1]:
-    for dayfile in listdir(path=join("aoc",year))[::-1]:
-        if not isfile(join("aoc",year,dayfile)):
-            continue
-        day = int(dayfile.split(".")[0])
+for year in listdir(path="aoc")[::-1]:
+    for dayfile in sorted(
+        glob(join("aoc", year, "*.py")), key=lambda x: -int(basename(x).split(".")[0])
+    ):
+        day = int(basename(dayfile).split(".")[0])
         mod = import_module(f"aoc.{year}.{day}", package=None)
         method = mod.main
 
@@ -22,16 +23,18 @@ for year in listdir(path='aoc')[::-1]:
             data = mod.demo_data
             expected_result = mod.demo_result
         else:
-            data = get_data(session,day,int(year),block=True)
+            data = get_data(session, day, int(year), block=True)
 
         res = method(data)
 
         if run_demo and res != expected_result:
-            raise ValueError(f"Invalid output for {year}/{day}: {res} != {expected_result}")
+            raise ValueError(
+                f"Invalid output for {year}/{day}: {res} != {expected_result}"
+            )
 
-        result_file = join(result_path,year,f"{day}.txt")
+        result_file = join(result_path, year, f"{day}.txt")
         result_folder = dirname(result_file)
         if not isdir(result_folder):
             makedirs(result_folder)
-        with open(result_file,"w") as fp:
+        with open(result_file, "w") as fp:
             fp.write(str(res))
