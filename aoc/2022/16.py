9ec8da20706bff0792012a0ec776a891eff275ef
Xavier TOLZA <tolza.xavier@gmail.com>
2022-12-27 07:41:38 +0100

    wip

--- aoc/2022/16.py
+++ aoc/2022/16.py
@@ -46,13 +46,15 @@ def main(data_str):
     distances = PyGraph(np.where(np.isinf(graph),0,graph.astype(int))).floyd_warshall()
 
     flows_cheat = np.where(np.arange(N)==0,flows.max()*10,flows)
-    selector = np.arange(N)[(flows_cheat>0)[np.argsort(-flows_cheat)]]
+    sorter = np.argsort(-flows_cheat)
+    selector = np.arange(N)[sorter][flows_cheat[sorter]>0]
 
     distances = distances[selector,:][:,selector]
     labels = labels[selector]
     flows = flows[selector]
     
     g = PyGraph(distances)
-    for paths in g.iter_paths_by_length():
-        raise NotImplementedError
+    for paths in g.iter_paths_by_length(as_numpy_array=True):
+        if paths.shape[1]>1:
+            raise NotImplementedError
     raise NotImplementedError
